9febbc47f0c33ca62230fba22d3d51ebf420b2e7
yh <sdjadygks@gmail.com>
2025-08-07 10:54:37 +0900

    ui 틀

--- CL-Project/src/main/java/com/example/CL/Project/ClProjectApplication.java
+++ CL-Project/src/main/java/com/example/CL/Project/ClProjectApplication.java
@@ -2,14 +2,20 @@ package com.example.CL.Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.scheduling.annotation.EnableAsync;
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
-@EnableScheduling
-@EnableAsync
+//@SpringBootApplication
+//@EnableScheduling
+//@EnableAsync
+@SpringBootApplication(exclude = {
+	    DataSourceAutoConfiguration.class,
+	    HibernateJpaAutoConfiguration.class
+	})
 public class ClProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClProjectApplication.class, args);
